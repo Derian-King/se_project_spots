93fb02c052904e906a1f6e15cf3b0125b11475b9
Derian King <derianking1422@gmail.com>
2025-08-25 19:29:26 -0500

    add function for open and close modals in js

--- scripts/index.js
+++ scripts/index.js
@@ -21,29 +21,37 @@ const newPostCardCaptionInput = newPostModal.querySelector(
 const profileNameEl = document.querySelector(".profile__name");
 const profileDescriptionEl = document.querySelector(".profile__description");
 
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal_is-opened");
+  openModal(editProfileModal);
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal_is-opened");
+  openModal(newPostModal);
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal_is-opened");
+  closeModal(newPostModal);
 });
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal_is-opened");
+  closeModal(editProfileModal);
 }
 
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
@@ -54,9 +62,9 @@ function handleNewPostSubmit(evt) {
     image: newPostCardImageInput.value,
     caption: newPostCardCaptionInput.value,
   };
-  console.log(inputValues.image);
-  console.log(inputValues.caption);
-  newPostModal.classList.remove("modal_is-opened");
+  console.log(inputValues);
+  closeModal(newPostModal);
+  evt.target.reset();
 }
 
 newPostForm.addEventListener("submit", handleNewPostSubmit);
